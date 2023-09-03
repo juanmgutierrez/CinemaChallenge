4ff35e8dabf149d0bae088f67d842d01e9d009bf
JM Gutiérrez <jmgutierrez.ar@gmail.com>
2023-09-03 20:06:57 -0300

    Changed the folders structure

---
+++ src/Cinema.Application/Showtime/Commands/CreateShowtimeCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace Cinema.Application.Showtime.Commands;
+
+// TODO Crear IDs tipados, no int
+public record CreateShowtimeCommand(int AuditoriumId, int MovieId, DateTimeOffset SessionDate);
